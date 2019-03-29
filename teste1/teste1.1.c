8db091b420daa3a2c3d72185059f6ad65d35ed52
Bruno Brandão <48556994+Buorn@users.noreply.github.com>
2019-03-29 20:37:27 -0300

    Create teste1.1.c

---
+++ teste1/teste1.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int num, soma;
+    soma = 0;
+
+    printf("Escolha um numero: ");
+    scanf("%d", &num);
+
+    while(num > 0) {
+        soma = soma +num;
+        num = (num - 1);
+    }
+        printf("somou %d \n", soma);
+    
+    return(0);
+}
